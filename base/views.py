a2d7a8f204fd3801b168784662c30da3fd853c5d
L4ZZA <pietro.lazz@gmail.com>
2020-02-29 13:04:55 +0000

    renamed home view to base

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'base/home.html')
+def base(request):
+    return render(request, 'base/base.html')
