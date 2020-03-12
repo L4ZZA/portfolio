870facb38e6a792220a159109f6304c1facc0f4c
L4ZZA <pietro.lazz@gmail.com>
2020-03-12 12:00:28 +0000

    Fixed settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,6 +130,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_resources'),
-    os.path.join(BASE_DIR, 'images'),
+    os.path.join(BASE_DIR, 'static_resources')
 ]
